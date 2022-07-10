cf2b0aa13fc8febb1bb536764bd0e1b451124ed7
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-10 20:44:26 +0700

    Save user in UsersDB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const { reset } = require('nodemon')
+const {
+  reset
+} = require('nodemon')
 const router = express.Router()
+
+const Users = require('../models/users')
 
 router.get("/", (req, res) => {
   res.redirect("./houses/list")
 })
 
 router.get("/login", (req, res) => {
-   res.render("login")
+  res.render("login")
 })
 
 router.get("/signup", (req, res) => {
@@ -23,7 +27,12 @@ router.post("/login", (req, res) => {
 })
 
 router.post("/signup", (req, res) => {
-  //.........
+  Users.create({
+    name: req.body.fullname,
+    email: req.body.email,
+    password: req.body.password,
+    avatar: req.body.picture
+  }), err => res.render("error")
 })
 
 
