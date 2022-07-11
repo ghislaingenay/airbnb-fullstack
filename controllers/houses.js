540c2a6a7893170f7a9c71e57a96a26f2cd446a9
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-11 16:37:12 +0700

    Create house - connection DB

--- controllers/houses.js
+++ controllers/houses.js
@@ -50,29 +50,28 @@ router.get("/:id/edit", (req, res) => {
   }
 })
 
-router.post("/", (req, res) => {
-  let user = req.user
-  if (req.isAuthenticated()){
-    res.render("./houses/list", {user: user})
-  } else {
-    res.redirect("/auth/login");
+router.post("/", async (req, res,next) => {
+  try {
+  let house = await Houses.create({
+    title: req.body.title,
+    description: req.body.description,
+    rooms: req.body.rooms,
+    location: req.body.location,
+    price: req.body.price,
+    photos: [req.body.picture1, req.body.picture2, req.body.picture3, req.body.picture4, req.body.picture5, req.body.picture6, req.body.picture7, req.body.picture8, req.body.picture9]
+  })
+  res.redirect("/houses/"+house._id)
+  } catch (err) {
+    next(err)
   }
 })
 
 router.patch("/:id", (req, res) => {
-  if (req.isAuthenticated()){
-    res.render("./houses/:id")
-  } else {
-    res.redirect("/auth/login");
-  }
+  
 })
 
 router.delete("/:id", (req, res) => {
-  if (req.isAuthenticated()){
-    res.render("./houses/:id")
-  } else {
-    res.redirect("/auth/login");
-  }
+  
 })
 
 module.exports = router
