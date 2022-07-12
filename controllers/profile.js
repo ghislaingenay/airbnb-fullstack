ada1aea377988ff3f1f30c331b1c1cf327c19649
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-12 14:08:50 +0700

    Add basics templating - Not completed

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,19 +1,47 @@
 const express = require('express')
 const router = express.Router()
 const Users = require('../models/users')
+const Houses = require('../models/houses')
 
-router.get("/", (req, res) => {
-  if (req.isAuthenticated()){
-    let user = req.user
-    res.render("profile", {user: user});
+
+router.get("/", async (req, res) => {
+  if (req.isAuthenticated()) {
+    let selectedHouses = await Houses.find({
+      host: req.user.name
+    })
+    res.render("profile", {
+      user: user,
+      houses: selectedHouses
+    })
   } else {
     res.redirect("/auth/login");
   }
- res.render("profile")
 })
 
-router.patch("/", (req, res) => {
-  //.........
- })
+router.patch("/", async (req, res, next) => {
+  try {
+    let foundUser = Users.findOne({
+      email: req.body.email
+    })
+    if (req.body.email === foundUser.email) {
+      throw new Error("This email is already taken")
+    } else {
+      let updatedUser = await Users.findOneAndUpdate({
+        email: req.body.email
+      }, {
+        name: req.body.prfname,
+        email: req.body.prfemail,
+        picture: req.body.newavatar,
+      })
+      res.redirect("profile", {
+        user: updatedUser
+      })
+    }
+  } catch (err) {
+    next(err)
+  }
+
+})
+
 
 module.exports = router
