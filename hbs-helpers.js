289cb5b90e40f6be5e91d5632f1fe0faf36fddd2
Ghislain Genay <ghislaingenay@gmail.com>
2022-07-14 13:40:28 +0700

    Add if greater than helper

--- hbs-helpers.js
+++ hbs-helpers.js
@@ -4,4 +4,8 @@ hbs.registerHelper('ifEq',(arg1, arg2, options) => {
   return String(arg1) == String(arg2) ? options.fn(this) : options.inverse(this)
 })
 
+hbs.registerHelper('ifGt',(arg1, arg2, options) => {
+  return Number(arg1) > Number(arg2) ? options.fn(this) : options.inverse(this)
+})
+
 module.exports = hbs
